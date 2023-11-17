69d0b2f4f0361a1699b54fbe3d1e7478f64942d7
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-17 10:50:05 +0600

    added carts get api

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ async function run() {
 
     // cart collection
     app.get('/carts', async(req, res) => {
-      
+      const result = await cartCollection.find().toArray();
+      res.send(result);
     })
     app.post('/carts', async(req, res) =>{
       const cartItem = req.body;
