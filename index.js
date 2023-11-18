fe415025fd380abb9acedd587f0ecdc548c35888
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-18 23:17:54 +0600

    check admin api

--- index.js
+++ index.js
@@ -70,7 +70,19 @@ async function run() {
       res.send(result);
     });
 
-    
+    app.get('/users/admin/:email', verifyToken, async(req, res) =>{
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({ message: 'forbidden access'})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
+    })
 
     app.get('/users', async(req, res) => {
       const result = await userCollection.find().toArray();
