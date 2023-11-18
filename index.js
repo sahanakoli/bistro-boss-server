4dec79e364462484f048afd4521f8dc698276401
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-18 23:20:07 +0600

    verify admin

--- index.js
+++ index.js
@@ -48,7 +48,17 @@ async function run() {
       })
     }
 
-    
+    // use verify admin after verifyToken
+    const verifyAdmin = async(req, res, next) =>{
+      const email = req.decoded.email;
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === 'admin';
+    if(!isAdmin){
+      return res.status(403).send({message: 'forbidden access'});
+    }
+    next();
+    }
 
 
     // jwt related api
